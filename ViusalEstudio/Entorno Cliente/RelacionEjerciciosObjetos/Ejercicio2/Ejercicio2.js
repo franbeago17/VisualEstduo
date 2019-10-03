04eac6d1f6cb534ff2e89aba814712156e938371
franbeago17 <franbeago17@gmail.com>
2019-10-03 14:55:17 +0200

    trhdfg

--- ViusalEstudio/Entorno Cliente/RelacionEjerciciosObjetos/Ejercicio2/Ejercicio2.js
+++ ViusalEstudio/Entorno Cliente/RelacionEjerciciosObjetos/Ejercicio2/Ejercicio2.js
@@ -19,5 +19,28 @@ var factura = {
     },
     formaPago : "asfdascdx"
 };
-
 alert("La cantidad a pagar es: " + factura.impTotal());
+
+
+
+
+
+/*var cliente = {
+    numCliente : "13212332",
+    dni : "123123123H",
+    nombre : "sdgsv",
+    domicilio : "fdgfdsssss",
+    cp : "12345",
+    ciudad : "gtyefsdf",
+    provincia : "fsgnt"
+}
+
+var producto = {
+    descProducto : "asdadvfvssvd asdcasd asecasca ",
+    precio : 5,
+    cantidad : 5,
+    impTotal : ()=>{
+        var total=factura.precio*factura.cantidad;
+        return total;
+    }
+}*/
