805c8b084444d3168155c3ce1f12200d4180986a
franbeago17 <franbeago17@gmail.com>
2019-10-07 12:52:26 +0200

    fh

--- ViusalEstudio/Entorno Cliente/RelacionEjerciciosObjetos/Ejercicio2/Ejercicio2.js
+++ ViusalEstudio/Entorno Cliente/RelacionEjerciciosObjetos/Ejercicio2/Ejercicio2.js
@@ -3,29 +3,10 @@ var factura = {
     direccion : "hehe",
     telefono : "12312342",
     nif : "12312313G",
-    numCliente : "13212332",
-    dni : "123123123H",
-    nombre : "sdgsv",
-    domicilio : "fdgfdsssss",
-    cp : "12345",
-    ciudad : "gtyefsdf",
-    provincia : "fsgnt",
-    descProducto : "asdadvfvssvd asdcasd asecasca ",
-    precio : 5,
-    cantidad : 5,
-    impTotal : ()=>{
-        var total=factura.precio*factura.cantidad;
-        return total;
-    },
     formaPago : "asfdascdx"
 };
-alert("La cantidad a pagar es: " + factura.impTotal());
 
-
-
-
-
-/*var cliente = {
+var cliente = {
     numCliente : "13212332",
     dni : "123123123H",
     nombre : "sdgsv",
@@ -33,14 +14,15 @@ alert("La cantidad a pagar es: " + factura.impTotal());
     cp : "12345",
     ciudad : "gtyefsdf",
     provincia : "fsgnt"
-}
+};
 
 var producto = {
     descProducto : "asdadvfvssvd asdcasd asecasca ",
     precio : 5,
     cantidad : 5,
     impTotal : ()=>{
-        var total=factura.precio*factura.cantidad;
+        var total=producto.precio*producto.cantidad;
         return total;
     }
-}*/
+};
+alert("La cantidad a pagar es: " + producto.impTotal());
