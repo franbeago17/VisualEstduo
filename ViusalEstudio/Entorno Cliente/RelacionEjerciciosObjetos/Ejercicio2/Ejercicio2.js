41c9a199776a7e30ef6dd8dc603ff199c29882d8
franbeago17 <franbeago17@gmail.com>
2019-10-08 14:08:59 +0200

    zdfd

--- ViusalEstudio/Entorno Cliente/RelacionEjerciciosObjetos/Ejercicio2/Ejercicio2.js
+++ ViusalEstudio/Entorno Cliente/RelacionEjerciciosObjetos/Ejercicio2/Ejercicio2.js
@@ -1,28 +1,66 @@
-var factura = {
-    nombreEmpresa : "Hola",
-    direccion : "hehe",
-    telefono : "12312342",
-    nif : "12312313G",
-    formaPago : "asfdascdx",
-    impTotal : ()=>{
-        var total=producto.precio*producto.cantidad;
-        return total;
+class Empresa {
+    constructor(nombre, direccion, telefono, nif) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.nif = nif;
     }
 };
 
-var cliente = {
-    numCliente : "13212332",
-    dni : "123123123H",
-    nombre : "sdgsv",
-    domicilio : "fdgfdsssss",
-    cp : "12345",
-    ciudad : "gtyefsdf",
-    provincia : "fsgnt"
+class Cliente {
+    constructor(numCliente, dni, nombre, domicilio, cp, ciudad, provincia) {
+        this.numCliente = numCliente;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+        this.cp = cp;
+        this.ciudad = ciudad;
+        this.provincia = provincia;
+    }
 };
 
-var producto = {
-    descProducto : "asdadvfvssvd asdcasd asecasca ",
-    precio : 5,
-    cantidad : 8
+class Producto {
+    constructor(descripcion, precio, cantidad) {
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
 };
-alert("La cantidad a pagar es: " + factura.impTotal());
+
+class Factura {
+    constructor(Empresa, Cliente, impTotal, formaPago) {
+        this.Empresa = empresa;
+        this.Cliente = cliente;
+        this.impTotal = impTotal;
+        this.formaPago = formaPago;
+    }
+}
+
+var empresa = new Empresa("FBG.SL", "C/ Navas", "123456789", "19283746J");
+var cliente = new Cliente("124578", "13579320K", "Huanito", "C/ Mezquita", "12678", "El Charco", "Pantano");
+var arrayProd = [new Producto("Silla", "35", "4"), new Producto("Mesa", "63", "1"), new Producto("Cristal", "20", "2")];
+var factura = new Factura(empresa, cliente, "2", "Tarjeta");
+
+function imprimir() {
+    document.write("<h1>Factura</h1>" +
+        "<h2>Empresa</h2>" +
+        "<u>Nombre de la empresa</u>: " + empresa.nombre + "<br/>" +
+        "<u>Dirección</u>: " + empresa.direccion + "<br/>" +
+        "<u>Telefono</u>: " + empresa.telefono + "<br/>" +
+        "<u>Nif</u>: " + empresa.nif + "<br/>");
+
+    document.write("<h2>Cliente</h2>" +
+        "<u>Nombre del cliente</u>: " + cliente.nombre + "<br/>" +
+        "<u>Número</u>: " + cliente.numCliente + "<br/>" +
+        "<u>DNI</u>: " + cliente.dni + "<br/>" +
+        "<u>Domicilio</u>: " + cliente.domicilio + "<br/>" +
+        "<u>Código Postal</u>: " + cliente.cp + "<br/>" +
+        "<u>Ciudad</u>: " + cliente.ciudad + "<br/>" +
+        "<u>Provincia</u>: " + cliente.provincia);
+
+    function impTotal() {
+        arrayProd.forEach(elemento => {
+            total = total + elemento.precio * elemento.cantidad
+        });
+    }
+}
