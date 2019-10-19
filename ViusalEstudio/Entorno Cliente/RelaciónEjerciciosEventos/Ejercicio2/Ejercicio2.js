8f16f83ea25be3d54fcb82ea24d920cbb55f6c79
franbeago17 <franbeago17@gmail.com>
2019-10-19 12:35:09 +0200

    sfd

---
+++ ViusalEstudio/Entorno Cliente/RelaciónEjerciciosEventos/Ejercicio2/Ejercicio2.js
@@ -0,0 +1,22 @@
+
+function ocultar(parrafo, enlace, enlace2){
+    document.getElementById(parrafo).style.display = "none";
+    document.getElementById(enlace2).style.visibility = "visible";
+    document.getElementById(enlace).style.visibility = "hidden";
+}
+function muestraOculta(parrafo, enlace, enlace2){
+    document.getElementById(parrafo).style.display = "block";
+    document.getElementById(enlace).style.visibility = "visible";
+    document.getElementById(enlace2).style.visibility = "hidden";
+}
+function aumentar(parrafo){
+    var tamaño = parseInt(document.getElementById(parrafo).style.fontSize);
+    document.getElementById(parrafo).style.fontSize = tamaño+1;
+}
+function disminuir(parrafo){
+    var tamaño = parseInt(document.getElementById(parrafo).style.fontSize);
+    document.getElementById(parrafo).style.fontSize = tamaño-1;
+}
+function defecto(parrafo){
+    document.getElementById(parrafo).style.fontSize = 16;
+}
