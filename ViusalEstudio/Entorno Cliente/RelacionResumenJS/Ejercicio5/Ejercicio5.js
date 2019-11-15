1bcc2e78a0e7e3c1cfa115cfc6b2ebeec8b4c0b3
franbeago17 <franbeago17@gmail.com>
2019-11-15 13:36:54 +0100

    rfgh
    cg

--- ViusalEstudio/Entorno Cliente/RelacionResumenJS/Ejercicio5/Ejercicio5.js
+++ ViusalEstudio/Entorno Cliente/RelacionResumenJS/Ejercicio5/Ejercicio5.js
@@ -2,19 +2,28 @@ var contador = 0;
 var x = "X";
 var i = 0;
 var j = 0;
+var numPremiados = new Array();
 var randomNum = 0;
-document.write("<table border='1px solid'>");
 
+for (i = 0; i < 6; i++){
+    randomNum = parseInt(Math.random()*(49-1)+1);
+    if (numPremiados.includes(randomNum)){
+        i--;
+    }else{
+        numPremiados[i] = randomNum;
+    }
+}
+
+document.write("<table border='1px solid'>");
 for (i = 1; i < 8; i++) {
     document.write("<tr>");
     for (j = 1; j < 8; j++) {
-        contador++;
-        if (randomNum != contador) {
-            document.write("<td>" + contador + "</td>");
+        if (numPremiados.includes(contador)){
+        document.write("<td>" + x + "</td>");
         }else{
-            document.write("<td>" + x + "</td>");
+            document.write("<td>" + contador + "</td>");
         }
-        
+        contador++;
     }
     document.write("</tr>");
 }
---
+++ ViusalEstudio/Entorno Cliente/RelacionResumenJS/Ejercicio7/Ejercicio7.js
@@ -0,0 +1,20 @@
+function cambiarColorRojo(){
+    if (document.getElementById("divRojo")){
+        document.body.style.backgroundColor = "red";
+    }
+}
+function cambiarColorVerde(){
+    if (document.getElementById("divVerde")){
+        document.body.style.backgroundColor = "green";
+    }
+}
+function cambiarColorAmarillo(){
+    if (document.getElementById("divAmarillo")){
+        document.body.style.backgroundColor = "yellow";
+    }
+}
+function cambiarColorGris(){
+    if (document.getElementById("divGris")){
+        document.body.style.backgroundColor = "grey";
+    }
+}
