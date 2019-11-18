9084f1db91d9fef98d9cbf097aa0f71aaf4eed5b
franbeago17 <franbeago17@gmail.com>
2019-11-18 10:39:32 +0100

    asd
    ds

--- ViusalEstudio/Entorno Cliente/RelacionResumenJS/Ejercicio8/Ejercicio8.js
+++ ViusalEstudio/Entorno Cliente/RelacionResumenJS/Ejercicio8/Ejercicio8.js
@@ -1,7 +1,48 @@
 function convertirPeso(){
-
+    if (document.getElementById("kgs").value.length == 0 && document.getElementById("gs").value.length == 0){
+        alert("Debe introducir al menos un dato para convertir.");
+    }else{
+        if (document.getElementById("kgs").value.length != 0){
+            var operacion = parseInt(document.getElementById("kgs").value) * 1000;
+            document.getElementById("gs").value = operacion;
+        }else{
+            var operacion = parseInt(document.getElementById("gs").value) / 1000;
+            document.getElementById("kgs").value = operacion;
+        }
+    }
 }
 
 function convertirDistancia(){
+    if (document.getElementById("kms").value.length == 0 && document.getElementById("ms").value.length == 0){
+        alert("Debe introducir al menos un dato para convertir.");
+    }else{
+        if (document.getElementById("kms").value.length != 0){
+            var operacion = parseInt(document.getElementById("kms").value) * 1000;
+            document.getElementById("ms").value = operacion;
+        }else{
+            var operacion = parseInt(document.getElementById("ms").value) / 1000;
+            document.getElementById("kms").value = operacion;
+        }
+    }
+}
 
+function desapareceKgs(){
+    if (document.getElementById("kgs").value.length != 0){
+        document.getElementById("Kgs").value = "";
+    }
+}
+function desapareceKms(){
+    if (document.getElementById("kms").value.length != 0){
+        document.getElementById("Kms").value = "";
+    }
+}
+function desapareceGs(){
+    if (document.getElementById("gs").value.length != 0){
+        document.getElementById("gs").value = "";
+    }
+}
+function desapareceMs(){
+    if (document.getElementById("ms").value.length != 0){
+        document.getElementById("ms").value = "";
+    }
 }
