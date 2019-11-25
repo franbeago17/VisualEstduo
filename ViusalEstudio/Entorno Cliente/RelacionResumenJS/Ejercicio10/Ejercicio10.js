f62d54bff478b41a6d10621cea3c4ecaab6b9a9f
franbeago17 <franbeago17@gmail.com>
2019-11-25 12:16:48 +0100

    zxczxc
    zxc

--- ViusalEstudio/Entorno Cliente/RelacionResumenJS/Ejercicio10/Ejercicio10.js
+++ ViusalEstudio/Entorno Cliente/RelacionResumenJS/Ejercicio10/Ejercicio10.js
@@ -2,21 +2,25 @@
 var a = prompt("Introduce el primer número");
 var b = prompt("Introduce el segundo número");
 
-Math.mcd = function(a,b){
+function mcd(a,b){
     if (a % b == 0){
         return b;
     }else{
-        return (b/a%b);
+        return Math.mcd(b,a%b);
     }
+}
+
+Math.mcd = function(a,b){
+    return mcd(a,b);
 };
+
+function mcm(a,b){
+    return a * b / Math.mcd(a,b);
+}
 
 Math.mcm = function(a,b){
-    if (a % b == 0){
-        return a * b / b;
-    }else{
-        return a * b / (b/a%b);
-    }
+    return mcm(a,b);
 };
 
-Math.mcm(a,b);
-Math.mcd(a,b);
+document.write("El máximo común divisor de " + a + " y " + b + " es: " + Math.mcd(a,b) + "<br>");
+document.write("El mínimo común múltiplo de " + a + " y " + b + " es: " + Math.mcm(a,b) + "<br>");
